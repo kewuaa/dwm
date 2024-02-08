1a43240608854f5645aa4864444331af94810b54
kewuaa <202321011111@std.uestc.edu.cn>
2024-02-08 21:15:46 +0800

    update colors

--- config.h
+++ config.h
@@ -8,15 +8,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "FiraCode Nerd Font:size=15" };
 static const char dmenufont[]       = "FiraCode Nerd Font:size=20";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char norm_fg[]         = "#828bb8";
+static const char norm_bg[]         = "#1b1d2b";
+static const char sele_fg[]         = "#444a73";
+static const char sele_bg[]         = "#c8d3f5";
+static const char sele_border[]     = "#ffc777";
 static const char *colors[][3]      = {
-    /*               fg         bg         border   */
-    [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-    [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+    /* fg bg border */
+    [SchemeNorm] = { norm_fg, norm_bg, norm_bg },
+    [SchemeSel]  = { sele_fg, sele_bg, sele_border },
 };
 
 /* tagging */
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sele_bg, "-sf", sele_fg, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *inclight[] = { "xbacklight", "-inc", "5", NULL };
 static const char *declight[] = { "xbacklight", "-dec", "5", NULL };
